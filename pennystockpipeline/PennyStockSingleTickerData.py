aea2ceb099c4c42099a855e05d99b6d1461a560f
Mohammad Aftab Uddin <mohammadaftabuddin@gmail.com>
2024-08-21 06:58:15 +0600

    Single Ticker Versions
    Single Ticker Versions

---
+++ pennystockpipeline/PennyStockSingleTickerData.py
@@ -0,0 +1,267 @@
+## #!/usr/bin/env python
+
+## PennyStockSingleTickerData
+## This pipeline performs the following operations on data:
+##  - Data Gathering
+##  - Data Cleaning and Transformation
+##  - (Later, mat be) Exploratory Data analysis (EDA)
+
+## ToDo: Allow specifying features list to be selected from the database table or should we use Torch Filter
+
+## Please refer to the PennyStockPipeline.md for details of this pipeline
+
+# import torchdata.datapipes as dp
+import csv, os, sys
+import sqlite3
+import numpy as np
+import pandas as pd
+from time import time, strftime, gmtime
+
+import matplotlib.pyplot as plt # Visualization 
+import matplotlib.dates as mdates # Formatting dates
+import seaborn as sns # Visualization
+
+from sklearn.preprocessing import MinMaxScaler
+
+import torch # Library for implementing Deep Neural Network 
+
+class PennyStockSingleTickerData():
+    
+    def __init__(self, database_name_with_path, table_name, impute=True, verbose=0) -> None:
+        self.verbose = verbose
+        
+        self.__load_data(database_name_with_path, table_name, impute)
+        self.scaler = MinMaxScaler(feature_range=(0,1))
+
+        #self.plot_data()
+        
+
+    def plot_data(self):
+        plt.rcParams['figure.figsize'] = [14, 4] 
+        
+        data_df = pd.DataFrame(self.data, columns = self.headers)
+        plt.plot(data_df['p_date'], data_df['volume_weighted_average'], label = "volume_weighted_average", color = "b")
+        
+        return self
+
+    def split_dataset(self, split=0.8, to_torch=True):
+        x, y = self.xs, self.ys
+
+        #self.train_data, self.test_data = self.psd.o_data[:train_size], self.psd.o_data[train_size:]
+        
+        train_size = int(len(x) * split)
+    
+        x_train, x_test = x[:train_size], x[train_size:]
+        y_train, y_test = y[:train_size], y[train_size:]
+        #np.array
+        print(f'x_train.shape: {x_train.shape}, y_train.shape: {y_train.shape}')
+        print(f'x_test.shape: {x_test.shape}, y_test.shape: {y_test.shape}')
+    
+        if to_torch:
+            x_train = torch.tensor(x_train, dtype=torch.float32)
+            y_train = torch.tensor(y_train, dtype=torch.float32)
+            x_test = torch.tensor(x_test, dtype=torch.float32)
+            y_test = torch.tensor(y_test, dtype=torch.float32)
+
+        #print(f'x_train.shape: {x_train.shape}, y_train.shape: {y_train.shape}')
+        #print(f'x_test.shape: {x_test.shape}, y_test.shape: {y_test.shape}')
+        
+        self.x_train = x_train
+        self.x_test = x_test
+        self.y_train = y_train
+        self.y_test = y_test
+        
+        return self
+
+    def create_sequences(self, sequence_length=36, prediction_length=36):
+        xs, ys = [], []
+        index = 0
+        count = 0
+        
+        normalized_data = self.normalized_data
+        self_data = pd.DataFrame(self.data, columns=self.headers)
+        
+        tickers = self_data['ticker_id']
+        dates = self_data['p_date']
+
+        while index < len(normalized_data) - sequence_length - prediction_length + 1:
+            # Check if sequence is within a single day
+            if dates[index] == dates[index + sequence_length] and tickers[index] == tickers[index + sequence_length]:
+                # If day == 2024-05-31, print
+                # if dates[index] == "2024-05-31":
+                # print("We got a sequence from", dates[index], "to", dates[index + sequence_length], "sequence-length is", (index + sequence_length) -index, tickers[index], tickers[index + sequence_length])
+                xs.append(normalized_data[index:index + sequence_length])
+                ys.append(normalized_data[index + sequence_length:index + sequence_length + prediction_length])
+                index += sequence_length
+                count += 1
+            else:  # Move index to the start of the next day
+                newindex = index
+                while dates[newindex] == dates[newindex + 1]:
+                    newindex += 1
+                newindex += 1
+                index = newindex
+        #print("Valid days:", count)
+
+        self.xs = np.array(xs)
+        self.ys = np.array(ys)
+
+        return self
+    
+    def normalize(self, columns_to_normalize=[]):
+        if (len(columns_to_normalize) == 0):
+            print(f'[INFO][PennyStockData]: No columns were supplied to be normalized {columns_to_normalize}. Provide columns as a list in columns_to_normalize')
+            return self
+
+        if (self.verbose == 2):
+            print(f'[INFO][PennyStockData]: Performing ticker-wise normalization on {columns_to_normalize}')
+
+        normalized_data = pd.DataFrame()
+        # First we normalize by each ticker as tickerwise, the wva differs a lot
+        dfx = pd.DataFrame(self.data, columns=self.headers)
+        
+        data_by_ticker = {}
+        max_value = 0
+        for ticker in dfx['ticker_id'].unique():
+            data_by_ticker[ticker] = dfx[dfx['ticker_id'] == ticker].copy()
+            for ctn in columns_to_normalize:
+                if ctn in dfx.columns:
+                    max_value = data_by_ticker[ticker][ctn].max()
+                    #data_by_ticker[ticker][ctn] = (data_by_ticker[ticker][ctn] / data_by_ticker[ticker][ctn].max()) ## doing inplace
+
+        self.max_value = max_value
+        print(f'max_value: {max_value}')
+        for ticker in data_by_ticker:
+            # create a temporary DataFrame to hold the current data
+            temp_df = pd.DataFrame(data_by_ticker[ticker].values, columns=data_by_ticker[ticker].keys())
+            normalized_data = pd.concat([normalized_data, temp_df], axis=0, ignore_index=True)
+    
+        # optionally, you can reset the index if needed
+        normalized_data.reset_index(drop=True, inplace=True)
+
+        if (self.verbose == 2):
+            print(f'[INFO][PennyStockData]: Performing global normalization on {columns_to_normalize} using MixMaxScaler')
+        
+        normalized_data[columns_to_normalize] = self.scaler.fit_transform(normalized_data[columns_to_normalize])
+        
+        #normalized_data = normalized_data.drop(columns=['ticker_id'])
+        #normalized_data.reset_index(drop=True, inplace=True)
+        
+        self.normalized_data = normalized_data[columns_to_normalize].values.tolist()
+        self.normalized_headers = columns_to_normalize
+        
+        return self
+
+    def get_columns(self, columns_as_list=[]):
+        if (len(columns_as_list) == 0):
+            return self
+        
+        df = pd.DataFrame(self.data, columns=self.headers)
+        
+        self.data = df[columns_as_list].values.tolist()
+        self.headers = columns_as_list
+
+        return self
+
+    def _get_imputed(self, data, headers) -> (list, list):
+        no_of_records = len(data)
+        total_imputed = 0
+        
+        # First row
+        time_diff = 0
+        data[0].append(time_diff)
+        
+        imputed_data = list()
+        imputed_data.append(data[0])
+        
+        for row_index in range(no_of_records-1):
+            if (data[row_index][0] == data[row_index+1][0] and data[row_index][2] == data[row_index+1][2]):
+                time_diff = (int)(((int)(data[row_index+1][9]) - (int)(data[row_index][9]))/(60))  # in mins
+            else:
+                time_diff = 0
+            
+            # IMPUTE
+            if (time_diff > 5):
+                rows_to_impute = (int)((time_diff - 5) / 5)
+                imputed_cursor = 1
+        
+                final_row = data[row_index+1].copy()
+                final_row.append(5.)
+                
+                for o in range(rows_to_impute):
+                    ## we need to insert this copied_record_from_previous_row after data[row_index]
+                    copied_record_from_previous_row = None
+                    copied_record_from_previous_row = data[row_index].copy()
+                    copied_record_from_previous_row[9] = (int)(copied_record_from_previous_row[9]) + (300 * imputed_cursor) ## add 5 mins
+                    copied_record_from_previous_row[3] = strftime("%H:%M", gmtime(((int)(copied_record_from_previous_row[9]))))
+                    copied_record_from_previous_row.append(5.)
+                    
+                    imputed_data.append(copied_record_from_previous_row)
+                    
+                    copied_record_from_previous_row = None
+                    imputed_cursor = imputed_cursor + 1
+                    
+                imputed_data.append(final_row)
+                total_imputed = total_imputed + rows_to_impute  
+            else:
+                copied_record_from_previous_row = data[row_index+1].copy()
+                copied_record_from_previous_row.append(time_diff)
+                imputed_data.append(copied_record_from_previous_row)
+            
+        headers.append('time-diff')
+
+        print(f'[DEBUG][PennyStockData]: Imputed len(data): {len(imputed_data)}')
+        
+        return imputed_data, headers
+
+    def __len__(self) -> int:
+        return len(self.data)
+
+    def __getitem__(self, idx):
+        return self.data[idx]
+        
+    def __load_data(self, path, table, impute):
+        assert self.__file_exists(path)
+        data = None
+        
+        sqliteConnection = sqlite3.connect(path)
+        cursor = sqliteConnection.cursor()
+    
+        query = "SELECT ticker_id, ticker, p_date, p_time, volume_weighted_average, open, close, high, low, time/1000 as time, volume, number_of_trades FROM " + table + " WHERE ticker_id = 9 ORDER BY ticker_id, p_date, p_time"
+        cursor.execute(query)
+        
+        data = [list(i) for i in cursor.fetchall()]
+        headers = list(map(lambda x: x[0], cursor.description))
+        
+        if impute:
+            data, headers = self._get_imputed(data, headers)
+
+        self.data = data
+        self.headers = headers
+
+        # get max next date from database
+        next_max_date = self.__get_next_max_date(table, cursor)
+        print(f'next_max_date: {next_max_date}')
+        self.next_max_date = next_max_date
+        
+        return self
+
+    def __get_next_max_date(self, table, cursor) -> str:
+        next_max_date = ""
+        query = "SELECT max(date(p_date, '+1 day')) as next_max_date FROM " + table + " WHERE 1"
+        cursor.execute(query)
+        
+        rs = cursor.fetchone()
+        if (rs is None):
+            raise Exception("Max Date could not be retrieved from DB")
+        else:
+            next_max_date = rs[0]
+
+        return next_max_date
+    
+    # Private valid file checker
+    def __file_exists(self, path) -> bool:
+        if os.path.isfile(path):
+            return True
+        else:
+            print("[INFO][PennyStockData]:", path, "not found")
+            return False
---
+++ pennystockpipeline/PennyStockSingleTickerModel.py
@@ -0,0 +1,238 @@
+## #!/usr/bin/env python
+
+## PennyStockSingleTickerModel
+## 
+import csv, os, sys
+import numpy as np
+import pandas as pd
+from time import time, strftime, gmtime
+from datetime import datetime
+
+import matplotlib.pyplot as plt # Visualization 
+import matplotlib.dates as mdates # Formatting dates
+import seaborn as sns # Visualization
+
+import torch # Library for implementing Deep Neural Network 
+import torch.nn as nn
+import torch.nn.functional as F
+import torch.optim as optim
+from torch.utils.data import TensorDataset, DataLoader
+
+from sklearn.preprocessing import MinMaxScaler
+
+from pennystockpipeline.PennyStockSingleTickerData import PennyStockSingleTickerData
+
+class PennyStockSingleTickerModel(nn.Module):
+    # input_size : number of features in input at each time step
+    # hidden_size : Number of LSTM units 
+    # num_layers : number of LSTM layers 
+    def __init__(self, input_size, hidden_size, num_layers, device='cpu') -> None: 
+        super(PennyStockSingleTickerModel, self).__init__() #initializes the parent class nn.Module
+        self.device = device
+        if device=='cuda':
+            self.device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+        
+        self.lstm = nn.LSTM(input_size, hidden_size, num_layers, batch_first=True) #.to(self.device)
+        self.linear = nn.Linear(hidden_size, 1)
+
+    def forward(self, x): # defines forward pass of the neural network       
+        out, _ = self.lstm(x)
+        out = self.linear(out)
+        
+        return out
+
+    def forecast(self, num_forecast_steps):
+        # Define the number of future time steps to forecast
+        self.num_forecast_steps = num_forecast_steps
+         
+        # Convert to NumPy and remove singleton dimensions
+        sequence_to_plot = self.x_test.squeeze().cpu().numpy()
+    
+        # Use the last 36 data points as the starting point
+        historical_data = sequence_to_plot[-1]
+        
+        # Initialize a list to store the forecasted values
+        forecasted_values = []
+         
+        # Use the trained model to forecast future values
+        with torch.no_grad():
+            for _ in range(num_forecast_steps * 2):
+                # Prepare the historical_data tensor
+                historical_data_tensor = torch.as_tensor(historical_data).view(1, -1, 1).float().to(self.device)
+                # Use the model to predict the next value
+                predicted_value = self(historical_data_tensor).to(self.device).numpy()[0, 0]
+                
+                # Append the predicted value to the forecasted_values list
+                forecasted_values.append(predicted_value[0])
+         
+                # Update the historical_data sequence by removing the oldest value and adding the predicted value
+                #historical_data
+                historical_data = np.roll(historical_data, shift=-1)
+                historical_data[-1] = predicted_value
+    
+        # Generate
+        model_psd_data_df = pd.DataFrame(self.psd.data, columns=self.psd.headers)
+        psd_ds_dates = model_psd_data_df['p_date'].values.tolist()
+        psd_ds_times = model_psd_data_df['p_time'].values.tolist()
+    
+        #psd_ds_dates_dt = [datetime.strptime(d, "%Y-%m-%d") for d in psd_ds_dates]
+        #last_date = max(psd_ds_dates_dt)
+        #next_date = last_date + timedelta(days=1)
+    
+        next_date = self.psd.next_max_date
+    
+        next_date_ls = [next_date for i in range(num_forecast_steps)]
+    
+        #print(type(psd_ds_dates), type(psd_ds_dates[0]))
+        #print(type(next_date_ls), type(next_date_ls[0]))
+    
+        psd_ds_dates = psd_ds_dates + next_date_ls
+    
+        #print(type(psd_ds_dates), type(psd_ds_dates[0]))
+        #print(type(next_date_ls), type(next_date_ls[0]))
+        
+        time_steps_set = ['16:30', '16:35', '16:40', '16:45', '16:50', '16:55', '17:00', '17:05', '17:10', '17:15', '17:20', '17:25', '17:30', '17:35', '17:40', '17:45', '17:50', '17:55', '18:00', '18:05']
+        psd_ds_times = psd_ds_times + time_steps_set
+    
+        #print(type(psd_ds_dates[0]), psd_ds_dates[0])
+        
+        # Concatenate the original index with the future dates
+        self.psd_ds_dates = psd_ds_dates
+        self.psd_ds_times = psd_ds_times
+    
+        self.forecasted_values = forecasted_values
+        self.sequence_to_plot = sequence_to_plot
+    
+        return self
+
+    def create_dataloaders(self, psd, batch_size=16):
+        # Create DataLoader for batch training
+        self.batch_size = batch_size
+
+        self.psd = psd
+        
+        self.x_train, self.y_train = psd.x_train, psd.y_train
+        self.x_test, self.y_test = psd.x_test, psd.y_test
+        
+        self.train_dataset = TensorDataset(self.x_train, self.y_train)
+        self.train_loader = DataLoader(self.train_dataset, batch_size = self.batch_size, shuffle=True) 
+        
+        # Create DataLoader for batch training
+        self.test_dataset = TensorDataset(self.x_test, self.y_test)
+        self.test_loader = DataLoader(self.test_dataset, batch_size = self.batch_size, shuffle=False)
+        
+        return self
+        
+    def train_model(self, loss_fn, optimizer, num_epochs=50):
+        model = self
+        #self.loss_fn = loss_fn
+        #self.optimizer = optimizer
+        
+        self.num_epochs = num_epochs
+        train_hist =[]
+        test_hist =[]
+        
+        # Training loop
+        for epoch in range(num_epochs):
+            total_loss = 0.0
+         
+            # Training
+            model.train()
+            for batch_x, batch_y in self.train_loader:
+                batch_x, batch_y = batch_x.to(self.device), batch_y.to(self.device)
+                predictions = model(batch_x)
+                #print(predictions)
+                loss = loss_fn(predictions, batch_y)
+         
+                optimizer.zero_grad()
+                loss.backward()
+                optimizer.step()
+         
+                total_loss += loss.item()
+         
+            # Calculate average training loss and accuracy
+            average_loss = total_loss / len(self.train_loader)
+            train_hist.append(average_loss)
+         
+            # Validation on test data
+            model.eval()
+            with torch.no_grad():
+                total_test_loss = 0.0
+         
+                for batch_x_test, batch_y_test in self.test_loader:
+                    batch_x_test, batch_y_test = batch_x_test.to(self.device), batch_y_test.to(self.device)
+                    predictions_test = model(batch_x_test)
+                    test_loss = loss_fn(predictions_test, batch_y_test)
+         
+                    total_test_loss += test_loss.item()
+         
+                # Calculate average test loss and accuracy
+                average_test_loss = total_test_loss / len(self.test_loader)
+                test_hist.append(average_test_loss)
+            if (epoch+1)%10==0:
+                print(f'Epoch [{epoch+1}/{num_epochs}] - Training Loss: {average_loss:.4f}, Test Loss: {average_test_loss:.4f}')
+
+        self.train_hist = train_hist
+        self.test_hist = test_hist
+        
+        return self
+
+    def plot_training_test_loss(self):
+        _x = np.linspace(1, self.num_epochs, self.num_epochs)
+        plt.plot(_x, self.train_hist, scalex=True, label="Training loss")
+        plt.plot(_x, self.test_hist, label="Test loss")
+        plt.legend()
+        plt.show()
+        
+        return self
+
+    def plot_forecasting(self):
+        forecasted_values = self.forecasted_values
+
+        psd_ds_dates = self.psd_ds_dates[-100:]
+        psd_ds_times = self.psd_ds_times[-100:]
+    
+        # last 100 rows
+        psd_ds_datetimes = []
+        
+        [psd_ds_datetimes.append(d + " " + t) for d,t in zip(psd_ds_dates, psd_ds_times)]
+        test_data_x = np.float32(self.psd.scaler.inverse_transform(self.psd.x_test.squeeze()).reshape(-1, 1).squeeze())
+        
+        sequence_to_plot = self.sequence_to_plot
+        
+        model_psd_data_df = pd.DataFrame(self.psd.data, columns=self.psd.headers)
+        
+        #set the size of the plot 
+        plt.rcParams['figure.figsize'] = [14, 4] 
+        
+        #Test data
+        plt.plot(psd_ds_datetimes[-80:-20], test_data_x[-60:], label = "input", color = "b") 
+        
+        #reverse the scaling transformation
+        original_cases = self.psd.scaler.inverse_transform(np.expand_dims(sequence_to_plot[-1], axis=0))#.flatten() 
+        #original_cases = self.psd.scaler.inverse_transform(np.expand_dims(sequence_to_plot, axis=0))#.flatten() 
+        original_cases = original_cases.reshape(-1, 1).squeeze()
+
+        #the historical data used as input for forecasting
+        #plt.plot(psd_ds_datetimes[-40:], test_data_x[-40:], label='actual values', color='green') 
+        plt.plot(psd_ds_datetimes[-40:-20], original_cases, label='actual values', color='green')
+        
+        #Forecasted Values 
+        #reverse the scaling transformation
+        forecasted_cases = self.psd.scaler.inverse_transform(np.expand_dims(forecasted_values, axis=0)).flatten()
+        # plotting the forecasted values
+        plt.plot(psd_ds_datetimes[-40:], forecasted_cases, label='forecasted values', color='red') 
+        
+        plt.xlabel('Time Step')
+        plt.ylabel('Value')
+    
+        plt.xticks(psd_ds_datetimes, psd_ds_datetimes, rotation='vertical')
+        plt.locator_params(axis='x', nbins=len(psd_ds_datetimes))
+        plt.tight_layout(pad=4)
+        plt.subplots_adjust(bottom=0.15)
+    
+        plt.legend()
+        plt.title('Time Series Forecasting')
+        plt.grid(True)
+
+        return self
